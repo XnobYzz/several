51e92b9f237b34283f79313409a6988b7d026abc
XnobYzz <168630483+XnobYzz@users.noreply.github.com>
2024-09-22 18:20:19 +0700

    thay ntns, tnd
    up!!

--- scripts/22.09.2024/xie.js
+++ scripts/22.09.2024/xie.js
@@ -1,13 +1,18 @@
 // author: XIE
 
+// thay đổi thành ngày tháng năm sinh bản thân | định dạng: ngày-tháng-năm
 let ntns = document.querySelector('#azt-asc-time-picker');
-let nsme = '01-01-2000'; // thay đổi thành ngày tháng năm sinh bản thân | định dạng: ngày-tháng-năm
+let nsme = '01-01-2000'; // thay đổi
 ntns.value = nsme;
 let event = new Event('input', { bubbles: true });
 ntns.dispatchEvent(event);
+// nhập tên người dùng
+const tnd = document.getElementById('azt-acs-username');
+tnd.removeAttribute('disabled');
+tnd.value = 'xietestscript'; // thay đổi
 var button = document.querySelector('button[azt-track-service="setting-general-CDosWd3k63"]');
 if (button) {
     button.click();
 }
 
-// thay đổi ngày tháng năm sinh: azota.vn
+// thay đổi thông tin tài khoản, ngày tháng năm sinh: azota.vn
